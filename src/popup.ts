6be0d230c4d8050ac000590ee076b549be434576
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-11-28 14:01:07 -0500

    feat: ✨ add UI for editing rewrites in storage

--- src/popup.ts
+++ src/popup.ts
@@ -1,1 +1,59 @@
 import "./popup.css";
+
+import { html, render } from "lit";
+import type { Storage } from "./types";
+
+let { rewrites } = await chrome.storage.sync.get<Storage>("rewrites");
+
+function saveToStorage() {
+  chrome.storage.sync.set<Storage>({ rewrites });
+}
+
+function rerender() {
+  console.log(rewrites);
+
+  const template = html`
+    ${rewrites.map(
+      (rewrite, i) =>
+        html`
+          <input
+            value=${rewrite.source}
+            @change=${(event: Event) => {
+              const input = event.target as HTMLInputElement;
+              rewrite.source = input.value;
+              saveToStorage();
+            }}
+          />
+          <input
+            value=${rewrite.destination}
+            @change=${(event: Event) => {
+              const input = event.target as HTMLInputElement;
+              rewrite.destination = input.value;
+              saveToStorage();
+            }}
+          />
+          <button
+            @click=${() => {
+              rewrites.splice(i, 1);
+              rerender();
+              saveToStorage();
+            }}
+          >
+            remove
+          </button>
+        `
+    )}
+    <button
+      @click=${() => {
+        rewrites.push({ source: "", destination: "" });
+        rerender();
+      }}
+    >
+      add
+    </button>
+  `;
+
+  render(template, document.body);
+}
+
+rerender();
