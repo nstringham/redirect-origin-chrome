b557d6457481965e1160c817388c6dfa172a1eee
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2024-12-01 02:48:27 -0500

    docs: ✏️ update readme description grammar

--- manifest.ts
+++ manifest.ts
@@ -3,7 +3,7 @@ import type { ManifestV3Export } from "@crxjs/vite-plugin";
 export const manifest: ManifestV3Export = {
   manifest_version: 3,
   name: "Redirect Origin",
-  description: "A Chrome extension to configure redirect one origin to another while preserving path",
+  description: "A Chrome extension to configure redirecting one origin to another while preserving path",
   version: "0.0.1",
   permissions: ["webNavigation", "storage"],
   action: { default_popup: "src/popup.html" },
