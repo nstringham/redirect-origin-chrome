edc27f4a1ae36fa71858feee941d6ae6935b9a81
Nate Stringham <44071655+nstringham@users.noreply.github.com>
2025-04-08 11:29:00 -0400

    refactor: ♻️ use version from package.json

--- manifest.ts
+++ manifest.ts
@@ -1,11 +1,12 @@
 import type { ManifestV3Export } from "@crxjs/vite-plugin";
+import { version } from "./package.json";
 
 export const manifest: ManifestV3Export = {
   manifest_version: 3,
   icons: { [128]: "src/logo.png" },
   name: "Redirect Origin",
   description: "A Chrome extension to configure redirecting one origin to another while preserving path",
-  version: "0.0.1",
+  version: version,
   permissions: ["webNavigation", "storage"],
   action: { default_popup: "popup.html" },
   background: { service_worker: "src/service-worker.ts", type: "module" },
